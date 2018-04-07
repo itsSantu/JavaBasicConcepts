92c53d05165f65717f42dda60b5a09d0013e913c
Apple <apple@Apples-MacBook-Pro.local>
2018-04-07 19:04:35 +0530

    GettingUserInput

---
+++ src/com/javabasic/concepts/Tutorial7GettingUserInput.java
@@ -0,0 +1,34 @@
+package com.javabasic.concepts;
+
+import java.util.Scanner;
+
+public class Tutorial7GettingUserInput {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter your name : ");
+
+		String thank = input.nextLine();
+
+		System.out.println("Your Name is: " + thank);
+		
+		scanner();
+
+	}
+
+	
+	public static void scanner()
+	{
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Please enter your age : ");
+		
+		int age = s.nextInt();
+		
+		System.out.println("Your age is : " + age);
+		
+		
+	}
+}
