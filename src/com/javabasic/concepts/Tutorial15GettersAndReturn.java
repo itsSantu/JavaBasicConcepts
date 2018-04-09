0db6b57e6409cb77b3b492b9c84784d0ff9a3c33
Apple <apple@Apples-MacBook-Pro.local>
2018-04-09 22:55:14 +0530

    GettersNReturns

--- src/com/javabasic/concepts/Tutorial15GettersAndReturn.java
+++ src/com/javabasic/concepts/Tutorial15GettersAndReturn.java
@@ -7,13 +7,11 @@ public class Tutorial15GettersAndReturn {
 		DemoClass2 demo = new DemoClass2();
 		demo.name = "Demo User";
 		demo.age = 5;
-		demo.relation = "son";
-		demo.status = "happy";
-		System.out.print("Using DemoClass Variables here" + "  " + demo.name + "\t" + " " + demo.age + "\t" + " "
-				+ demo.relation + "\t" + " " + demo.status);	
+		
+		System.out.print("Using DemoClass Variables here" + "  " + demo.name + "\t" + " " + demo.age);	
 		System.out.println();
 		 
-		int r = demo.returns();
+		int r = demo.returnMethod();
 		System.out.println(r);
 		
 		
@@ -28,11 +26,12 @@ class DemoClass2 {
 	String relation;
 	String status;
 
-   int  returns() {
+   int  returnMethod() {
 	
 	System.out.println("My name is"+ " " + name + "  " + "My age is" +" " + age + " years of old");
 	
 	int yearsleft =60 -age;
+	
 	System.out.println(yearsleft);
 	
 	return yearsleft;
